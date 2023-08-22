0eaf97bbdca9c3d1d404c27eaf7fc673180597e4
Chris Stantis <chris.stantis@gmail.com>
2023-08-22 17:06:42 -0600

    Update setup.R

--- code/setup.R
+++ code/setup.R
@@ -1,5 +1,5 @@
-library(dplyr); library(googlesheets4); library(tidyr)
-library(stringr)
+library(dplyr); library(googlesheets4); library(tidyr);
+library(stringr); library(ggplot2)
 
 wide <- read_sheet('https://docs.google.com/spreadsheets/d/1h6h5yPlmJBaqjx_mOwyS9ETybus44Z3kRqqMZBp80zw')
 
@@ -25,4 +25,69 @@ df <- df%>%
 # DPAA bake
 # DPAA temp
 # DPAA UU untreated
-# DPAA UU ba
+# 
+# Treated versus Untreated ------------------------------------------------
+treatment <- df %>% 
+  filter(
+    str_detect(type, 'baked', negate = T)
+  )
+
+treatment$dC.off = treatment$dO.off = rep(0)
+
+sid = unique(treatment$sample)
+
+for(i in sid){
+  treatment$dC.off[treatment$sample == i] = 
+    treatment$d13C[treatment$sample == i & treatment$treated == T] - 
+    treatment$d13C[treatment$sample == i & treatment$treated == F]
+  
+}
+
+sid = unique(treatment$sample)
+for(i in sid){
+  treatment$dO.off[treatment$sample == i] = 
+    treatment$d18O[treatment$sample == i & treatment$treated == T] - 
+    treatment$d18O[treatment$sample == i & treatment$treated == F]
+  
+}
+
+treatmentCUU <- treatment %>% 
+  filter(iso == "C" & lab == "UU") %>% 
+  distinct(sample, .keep_all = T)
+
+treatmentCDPAA <- treatment %>% 
+  filter(iso == "C" & lab == "DPAA") %>% 
+  distinct(sample, .keep_all = T)  
+treatmentC <- rbind(treatmentCDPAA, treatmentCUU)
+
+ggplot() + 
+  geom_hline(yintercept = 0, color = 'grey20', linetype = 2) +
+  geom_boxplot(data = treatmentC, aes(x = lab, y = dC.off, fill = lab, color = lab)) + 
+  theme_classic() +
+  scale_fill_manual(values = c("pink", "purple")) +
+  scale_color_manual(values = c("#F05039", "#1F449C")) +
+  theme(legend.position = "none", 
+        axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12),
+        axis.title = element_text(size = 14), ) +
+  labs(x = "Treatment", 
+       y = expression(paste(Delta^13, "C", " (\u2030, VPDB)")))
+
+# Delta Calculated from Excel ---------------------------------------------
+
+delta <- read_sheet('https://docs.google.com/spreadsheets/d/1Xah5oyJbzPvUrd-tPMr9mOExBpUDIu0AhFo7mUxgfOQ')
+  
+  
+ ggplot() + 
+   geom_hline(yintercept = 0, color = 'grey20', linetype = 2) +
+   geom_boxplot(data = delta, aes(x = Treat, y = dO.off, fill = Treat, color = Time)) + 
+   theme_classic() +
+   scale_fill_manual(values = palette) +
+   scale_color_manual(values = c("#F05039", "#1F449C")) +
+   theme(legend.position = "none", 
+         axis.text.x = element_text(size = 12),
+         axis.text.y = element_text(size = 12),
+         axis.title = element_text(size = 14), ) +
+   labs(x = "Treatment", 
+        y = expression(paste(Delta^18, "O", " (\u2030, VPDB)")))
+  
