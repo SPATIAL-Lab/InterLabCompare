ead5d329ece276c560e98c398cd9594e37958fa5
Chris Stantis <chris.stantis@gmail.com>
2023-09-07 16:18:38 -0600

    Amending cleaning

--- code/setup.R
+++ code/setup.R
@@ -1,244 +1,62 @@
 library(dplyr); library(googlesheets4); library(tidyr);
 library(stringr); library(ggplot2)
 
-wide <- read_sheet('https://docs.google.com/spreadsheets/d/1h6h5yPlmJBaqjx_mOwyS9ETybus44Z3kRqqMZBp80zw')
+long <- read_sheet('https://docs.google.com/spreadsheets/d/1h6h5yPlmJBaqjx_mOwyS9ETybus44Z3kRqqMZBp80zw')
 
-df <- wide %>% 
-  pivot_longer(!Sample, 
-               names_to = 'type', 
-               values_to = 'value')
-
-df <- df%>% 
-  mutate(lab = ifelse(str_detect(type, 'utah'), 'UU', 'DPAA'), 
-         iso = ifelse(str_detect(type, 'd13c'), 'C', 'O'), 
-         treated = ifelse(str_detect(type, 'untreated'), F, T),
-         temp = case_when(str_detect(type, '30') ~ '30', 
-                          str_detect(type, '50') ~ '50'), 
-         baked = ifelse(str_detect(type, 'baked'), T, F), #assuming it's 30 for now
-         d18O = ifelse(str_detect(type, 'd18o'), value, NA), 
-         d13C = ifelse(str_detect(type, 'd13c'), value, NA)
-         ) %>%
-  rename(sample = Sample)
-
-write.csv(df, file = "singlevalues.csv")
-# First looks at sample values --------------------------------------------
-df %>% group_by(sample) %>% 
-  summarise(Count= n(),Mean=mean(d13C, na.rm = T),SD=sd(d13C, na.rm = T))
-
-df %>% group_by(sample) %>% 
-  summarise(Count= n(),Mean=mean(d18O, na.rm = T),SD=sd(d18O, na.rm = T))
-#is that a lot of oxygen variance?
-
-#okay next up we want columns of the offset
-#between samples (Delta) of: DPAA bake
-
-
-# DPAA UU untreated -------------------------------------------------------
-treatment <- df %>% 
-  filter(treated == F & is.na(temp) & baked == F)
-
-treatment$dC.off = treatment$dO.off = rep(0)
-
-treatmentC <- treatment %>% 
-  filter(iso == "C")
-
-sid = unique(treatment$sample)
-for(i in sid){
-  treatmentC$dC.off[treatmentC$sample == i] = 
-    treatmentC$d13C[treatmentC$sample == i & treatmentC$lab == 'DPAA'] - 
-    treatmentC$d13C[treatmentC$sample == i & treatmentC$lab == 'UU']
-}
-
-treatmentO <- treatment %>% 
-  filter(iso == "O")
-
-sid = unique(treatmentO$sample)
-for(i in sid){
-  treatmentO$dO.off[treatmentO$sample == i] = 
-    treatmentO$d18O[treatmentO$sample == i & treatmentC$lab == 'DPAA'] - 
-    treatmentO$d18O[treatmentO$sample == i & treatmentC$lab == 'UU']
-}
-
-treatmentC <- treatmentC %>%  
-  distinct(sample, .keep_all = T)  
-treatmentC$compare = 'DPAA-UU Untreated'
-
-treatmentO <- treatmentO %>%  
-  distinct(sample, .keep_all = T)  
-
-treatmentO$compare = 'DPAA-UU Untreated'
-
-ggplot() + 
-  geom_hline(yintercept = 0, color = 'grey20', linetype = 2) +
-  geom_boxplot(data = treatmentO, aes(y = dO.off, fill = lab)) + 
-  theme_classic() +
-  scale_fill_manual(values = c("pink", "purple")) +
-  scale_color_manual(values = c("#F05039", "#1F449C")) +
-  theme(legend.position = "none", 
-        axis.text.x = element_text(size = 12),
-        axis.text.y = element_text(size = 12),
-        axis.title = element_text(size = 14), ) +
-  labs(x = "Lab Comparison, untreated", 
-       y = expression(paste(Delta^18, "O", " (\u2030, VPDB)")))
-
-overallO <- treatmentO
-overallC <- treatmentC
-# 50-30 comparison WIP---------------------------------------------------------------
-# hrm the 50-30 comparison is actually four types of comparisons: 50-30 untreated or 50-30 treated, 
-# or 50 T-U *or* 30 T-U
-
-# ugh so first 50-30 untreated
-treatment <- df %>% 
-  filter(!is.na(temp) & treated == F)
-
-treatment$dC.off = treatment$dO.off = rep(0)
-# note that Utah only baked at 30, then compared treated and untreated
-treatment5030un <- treatment %>% 
-  filter(iso == "C")
-
-sid = unique(treatmentCUU$sample)
-
-for(i in sid){
-  treatmentCUU$dC.off[treatmentCUU$sample == i] = 
-    treatmentCUU$d13C[treatmentCUU$sample == i & treatmentCUU$temp == 50] - 
-    treatmentCUU$d13C[treatmentCUU$sample == i & treatmentCUU$temp == 30]
-}
-
-treatmentOUU <- treatment %>% 
-  filter(iso == "O" & lab == 'UU')
-
-sid = unique(treatmentOUU$sample)
-for(i in sid){
-  treatmentOUU$dO.off[treatment$sample == i] = 
-    treatmentOUU$d18O[treatment$sample == i & treatmentOUU$treated == T] - 
-    treatmentOUU$d18O[treatment$sample == i & treatmentOUU$treated == F]
-}
-
-treatmentCDPAA <- treatment %>% 
-  filter(iso == "C" & lab == "DPAA")
-sid = unique(treatmentCUU$sample)
-
-for(i in sid){
-  treatmentCDPAA$dC.off[treatmentCDPAA$sample == i] = 
-    treatmentCDPAA$d13C[treatmentCDPAA$sample == i & treatmentCDPAA$treated == T] - 
-    treatmentCDPAA$d13C[treatmentCDPAA$sample == i & treatmentCDPAA$treated == F]
-  
-}
-
-treatmentODPAA <- treatment %>% 
-  filter(iso == "O" & lab == 'DPAA')
-sid = unique(treatmentODPAA$sample)
-for(i in sid){
-  treatmentODPAA$dO.off[treatmentODPAA$sample == i] = 
-    treatmentODPAA$d18O[treatmentODPAA$sample == i & treatmentODPAA$treated == T] - 
-    treatmentODPAA$d18O[treatmentODPAA$sample == i & treatmentODPAA$treated == F]
-  
-}
-
-treatmentC <- rbind(treatmentCDPAA, treatmentCUU)
-treatmentC <- treatmentC %>% 
-  group_by(lab) %>% 
-  distinct(sample, .keep_all = T)  
-
-treatmentC$compare = 'Treated-Untreated'
-
-treatmentO <- rbind(treatmentODPAA, treatmentOUU)
-treatmentO <- treatmentO %>% 
-  group_by(lab) %>% 
-  distinct(sample, .keep_all = T)  
-
-treatmentO$compare = 'Treated-Untreated'
-
-ggplot() + 
-  geom_hline(yintercept = 0, color = 'grey20', linetype = 2) +
-  geom_boxplot(data = treatmentO, aes(x = lab, y = dO.off, fill = lab, color = lab)) + 
-  theme_classic() +
-  scale_fill_manual(values = c("pink", "purple")) +
-  scale_color_manual(values = c("#F05039", "#1F449C")) +
-  theme(legend.position = "none", 
-        axis.text.x = element_text(size = 12),
-        axis.text.y = element_text(size = 12),
-        axis.title = element_text(size = 14), ) +
-  labs(x = "Treatment", 
-       y = expression(paste(Delta^18, "O", " (\u2030, VPDB)")))
-
-# Treated versus Untreated ------------------------------------------------
-treatment <- df %>% 
-  filter(
-    str_detect(type, 'baked', negate = T)
+df <- long %>% 
+  mutate(lab = ifelse(str_detect(treatment, 'utah'), 'UU', 'DPAA'), 
+         treated = ifelse(str_detect(treatment, 'untreated'), F, T),
+         temp = case_when(str_detect(treatment, '30') ~ '30', 
+                          str_detect(treatment, '50') ~ '50'), 
+         baked = ifelse(str_detect(treatment, 'baked'), T, F), #assuming it's 30 for now
+         treatment = substring(treatment, 6)
   ) 
 
-treatment$dC.off = treatment$dO.off = rep(0)
+write.csv(df, file = "singlevalues.csv")
 
-treatmentCUU <- treatment %>% 
-  filter(iso == "C" & lab == "UU")
+# Intra-lab comparison, treated-untreated--------------------------------------
+uu <- df %>% 
+  filter(lab == 'UU')
 
-sid = unique(treatmentCUU$sample)
+uu$dC.off = uu$dO.off = rep(0)
 
+sid = unique(uu$sample)
 for(i in sid){
-  treatmentCUU$dC.off[treatmentCUU$sample == i] = 
-    treatmentCUU$d13C[treatmentCUU$sample == i & treatmentCUU$treated == T] - 
-    treatmentCUU$d13C[treatmentCUU$sample == i & treatmentCUU$treated == F]
+  uu$dC.off[uu$sample == i] = 
+    uu$d13C[uu$sample == i] - 
+    uu$d13C[uu$sample == i & uu$treatment == 'untreated']
 }
 
-treatmentOUU <- treatment %>% 
-  filter(iso == "O" & lab == 'UU')
-
-sid = unique(treatmentOUU$sample)
 for(i in sid){
-  treatmentOUU$dO.off[treatment$sample == i] = 
-    treatmentOUU$d18O[treatment$sample == i & treatmentOUU$treated == T] - 
-    treatmentOUU$d18O[treatment$sample == i & treatmentOUU$treated == F]
+  uu$dO.off[uu$sample == i] = 
+    uu$d18O[uu$sample == i] - 
+    uu$d18O[uu$sample == i & uu$treatment == 'untreated']
 }
 
-treatmentCDPAA <- treatment %>% 
-  filter(iso == "C" & lab == "DPAA")
-sid = unique(treatmentCUU$sample)
+dpaa <- df %>% 
+  filter(lab == 'DPAA')
 
+dpaa$dC.off = dpaa$dO.off = rep(0)
+
+sid = unique(uu$sample)
 for(i in sid){
-  treatmentCDPAA$dC.off[treatmentCDPAA$sample == i] = 
-    treatmentCDPAA$d13C[treatmentCDPAA$sample == i & treatmentCDPAA$treated == T] - 
-    treatmentCDPAA$d13C[treatmentCDPAA$sample == i & treatmentCDPAA$treated == F]
-  
+  dpaa$dC.off[dpaa$sample == i] = 
+    dpaa$d13C[dpaa$sample == i] - 
+    dpaa$d13C[dpaa$sample == i & dpaa$treatment == 'untreated']
 }
 
-treatmentODPAA <- treatment %>% 
-  filter(iso == "O" & lab == 'DPAA')
-sid = unique(treatmentODPAA$sample)
 for(i in sid){
-  treatmentODPAA$dO.off[treatmentODPAA$sample == i] = 
-    treatmentODPAA$d18O[treatmentODPAA$sample == i & treatmentODPAA$treated == T] - 
-    treatmentODPAA$d18O[treatmentODPAA$sample == i & treatmentODPAA$treated == F]
-  
+  dpaa$dO.off[dpaa$sample == i] = 
+    dpaa$d18O[dpaa$sample == i] - 
+    dpaa$d18O[dpaa$sample == i & dpaa$treatment == 'untreated']
 }
 
-treatmentC <- rbind(treatmentCDPAA, treatmentCUU)
-treatmentC <- treatmentC %>% 
-  group_by(lab) %>% 
-  distinct(sample, .keep_all = T)  
+df <- rbind(uu, dpaa)
+write.csv(df, file = "intralab.csv")
 
-treatmentC$compare = 'Treated-Untreated'
-
-treatmentO <- rbind(treatmentODPAA, treatmentOUU)
-treatmentO <- treatmentO %>% 
-  group_by(lab) %>% 
-  distinct(sample, .keep_all = T)  
-
-treatmentO$compare = 'Treated-Untreated'
-
-ggplot() + 
+ggplot(data = subset(df, treatment != 'untreated'), aes(x = treatment, y = dC.off, fill = lab)) + 
   geom_hline(yintercept = 0, color = 'grey20', linetype = 2) +
-  geom_boxplot(data = treatmentO, aes(x = lab, y = dO.off, fill = lab, color = lab)) + 
-  theme_classic() +
-  scale_fill_manual(values = c("pink", "purple")) +
-  scale_color_manual(values = c("#F05039", "#1F449C")) +
-  theme(legend.position = "none", 
-        axis.text.x = element_text(size = 12),
-        axis.text.y = element_text(size = 12),
-        axis.title = element_text(size = 14), ) +
-  labs(x = "Treatment", 
-       y = expression(paste(Delta^18, "O", " (\u2030, VPDB)")))
-
+  geom_boxplot()
 # Delta Calculated from Excel ---------------------------------------------
 
 delta <- read_sheet('https://docs.google.com/spreadsheets/d/1Xah5oyJbzPvUrd-tPMr9mOExBpUDIu0AhFo7mUxgfOQ')
@@ -259,10 +77,10 @@ long <- delta %>%
                        'DO_lab_untreated' = 'Untreated', 
                        'DC_lab_treated' = 'Treated', 
                        'DO_lab_treated' = 'Treated', 
-                       'DC_lab_untreated_30' = 'Untreated (30)', #something's up with this one...
-                       'DO_lab_untreated_30' = 'Untreated (30)',
-                       'DC_lab_untreated_50' = 'Untreated (50)', 
-                       'DO_lab_untreated_50' = 'Untreated (50)',
+                       'DC_lab_untreated_30' = 'Untreated, Baked, 30 Rxn Temp', #something's up with this one...
+                       'DO_lab_untreated_30' = 'Untreated, Baked, 30 Rxn Temp',
+                       'DC_lab_untreated_50' = 'Untreated, 50 Rxn Temp', 
+                       'DO_lab_untreated_50' = 'Untreated, 50 Rxn Temp',
                        
                        'DC_dpaa_treated_50v30' = '50-30, Treated',
                        'DO_dpaa_treated_50v30' = '50-30, Treated',
@@ -280,7 +98,7 @@ long <- delta %>%
                        'DC_utah_treated_untreated' = 'Treated-Untreated', 
                        'DO_utah_treated_untreated' = 'Treated-Untreated', 
                        'DC_utah_treated_baking' = 'Baking-Not Baking, Treated', 
-                       'DO_utah_treated_baking' = 'Baking-Not Baking, Treated',
+                       'DO_utah_treated_baking' = 'Baking-Not Baking, Treated'
                         ) 
          )
 
--- code/singleValues.R
+++ code/singleValues.R
@@ -1,11 +1,14 @@
-library(dplyr); library(tidyr);library(ggplot2)
+library(dplyr); library(tidyr);library(ggplot2); library(stringr)
 
 delta <- read.csv('delta.csv')
 sv <- read.csv('singlevalues.csv')
+il <- read.csv('intralab.csv') %>% 
+  filter(treatment != 'untreated')
+cols <- c("DPAA" = "#97c2f7",  "interlab" = "#5474C0", "UU" = "#2a3f70")
+cols2 <- c("O" = "#131F71", "C" = "#9CA4f8")
 #how do the samples look, regardless of treatment? 
 summ <- sv %>% 
-summ <- sv %>% 
-  group_by(iso, lab, sample) %>% 
+  group_by(lab, sample) %>% 
   summarize(mean = mean(value), 
             sd = sd(value), 
             min = min(value),
@@ -15,13 +18,13 @@ ggplot() +
   geom_point(data = summ, aes(x = sd, y = mean, color = sample, shape = iso))
 
 ggplot() + 
-  geom_col(data = summ, aes(y = sd, x = sample, color = iso, fill = lab), position = "dodge") + 
+  geom_col(data = summ, aes(y = sd, x = sample, color = iso, fill = lab), position = "dodge") +
+  scale_fill_manual(values = cols) + 
   theme_classic()
-
 
 ggplot() + 
   geom_hline(yintercept = 0, color = 'grey20', linetype = 2) +
-  geom_boxplot(data = long, aes(x = type, y = value, fill = iso, color = iso)) + 
+  geom_boxplot(data = delta, aes(x = type, y = value, fill = iso, color = iso)) + 
   theme_classic() +
   theme(axis.text.x = element_text(size = 12),
         axis.text.y = element_text(size = 12),
@@ -31,43 +34,120 @@ ggplot() +
        y = expression(paste(Delta, "isotope value", " (\u2030)")))
 # what do we only have one isotope set for still? 
 
-cols <- c("DPAA" = "#a0dee1",  "interlab" = "#7a1e47", "UU" = "#d2042d")
-cols2 <- c("O" = "#07C3D9", "C" = "#8C044D")
 #exploring interlab variability
 ggplot() + 
   geom_hline(yintercept = 0, color = 'grey20', linetype = 2) +
-  geom_boxplot(data = subset(long, lab == 'interlab'), 
+  geom_boxplot(data = subset(delta, lab == 'interlab'), 
                aes(x = type, y = value, fill = iso)) + 
   theme_classic() +
-<<<<<<< HEAD
   scale_fill_manual(values = cols2, 
                     name = "Isotope", 
                     labels = c(expression(paste(delta^13, 'C')), 
                                expression(paste(delta^18, 'O')))) + 
-=======
-  scale_fill_manual(values = cols2) + 
->>>>>>> fc057a2cdc74193f36df4efbc81461f6d75af732
   theme(axis.text.x = element_text(size = 12),
         axis.text.y = element_text(size = 12),
         axis.title = element_text(size = 14), ) + 
-  scale_x_discrete(guide = guide_axis(angle = 90)) +
-  #scale_fill_discrete(name = "Isotope", labels = c(expression(paste(delta^13, 'C')), expression(paste(delta^18, 'O')))) + 
+  scale_x_discrete(
+    labels = function(x) str_wrap(x, width = 10) 
+                   ) +
   labs(x = "Protocol", 
        y = expression(paste(Delta, " isotope value ", "(\u2030)")))
 
 # exploring DPAA variability
 ggplot() + 
   geom_hline(yintercept = 0, color = 'grey20', linetype = 2) +
-  geom_boxplot(data = subset(long, lab != 'interlab' & iso == 'C'), aes(x = type, y = value, fill = lab)) + 
+  geom_boxplot(data = subset(delta, lab == 'DPAA'), aes(x = type, y = value, fill = iso)) + 
   theme_classic() +
-  scale_fill_manual(values = cols, 
+  scale_fill_manual(values = cols2, 
                     name = "Isotope", 
-                    labels = c("DPAA", "U of U")
-                    ) + 
+                    labels = c(expression(paste(delta^13, 'C')), 
+                               expression(paste(delta^18, 'O')))) + 
   theme(axis.text.x = element_text(size = 12),
         axis.text.y = element_text(size = 12),
-        axis.title = element_text(size = 14), ) + 
-  scale_x_discrete(guide = guide_axis(angle = 90)) +
+        axis.title = element_text(size = 14), ) +   
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) +
   labs(x = "Protocol", 
        y = expression(paste(Delta, "isotope value", " (\u2030)")))
+
+# crazy test
+ggplot() + 
+  geom_boxplot(data = subset(sv, iso == 'C'), aes(x = type, y = value, fill = iso)) + 
+  theme_classic() +
+  scale_fill_manual(values = cols2, 
+                    name = "Isotope", 
+                    labels = c(expression(paste(delta^13, 'C')), 
+                               expression(paste(delta^18, 'O')))) + 
+  theme(axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12),
+        axis.title = element_text(size = 14), ) +   
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) +
+  labs(x = "Protocol", 
+       y = expression(paste(Delta, "isotope value", " (\u2030)")))
+
+# MMD and RID -------------------------------------------------------------
+#according to Pestle, MMD is mean pairwise difference + 4(avg standard deviations of each laboratory)
+
+#calculating mean pairwise difference using delta df
+mpdO <- round(mean(subset(delta, lab == 'interlab' & iso == 'O')$value), 2)
+mpdC <- round(mean(subset(delta, lab == 'interlab' & iso == 'C')$value), 2)
+sdUUO <- round(sd(subset(sv, lab == 'UU' & iso =='O')$value), 2)
+sdUUC <- round(sd(subset(sv, lab == 'UU' & iso =='C')$value), 2)
+sdDPAAO <- round(sd(subset(sv, lab == 'DPAA' & iso =='O')$value), 2)
+sdDPAAC <- round(sd(subset(sv, lab == 'DPAA' & iso =='C')$value), 2)
+
+
+# Descriptive Stats -------------------------------------------------------
+
+#are the data normally distributed? 
+names <- unique(sv$sample)
+for (name in names) {
+  print(shapiro.test(subset(sv, sample == name)$d18O))
+}
+for (name in names) {
+  print(shapiro.test(subset(sv, sample == name)$d13C))
+}#nothing is significant in these outputs
+rm(name, names)
+#mean and sd of Delta values by protocol
+delta %>% 
+  filter(lab == 'interlab') %>% 
+  group_by(type,iso) %>% 
+  summarize(mean = mean(value), 
+                 sd = sd(value))
+
+# Interlab stats ----------------------------------------------------------
+summDelta <- subset(delta, lab == 'interlab') %>% 
+  group_by(iso, type) %>% 
+  summarize(mean = round(mean(value), 2), 
+            sd = round(sd(value), 2), 
+            min = min(value),
+            max = max(value))
+
+shapiro.test(subset(delta, iso == 'O')$value)
+shapiro.test(subset(delta, iso == 'C')$value)
+ggplot(subset(delta, iso == 'O'), aes(value)) + geom_density()
+ggplot(subset(delta, iso == 'C'), aes(value)) + geom_density()
+
+treats = unique(subset(delta, lab == 'interlab')$type)
+shapTable = data.frame(treats, "C" = rep(0), "O" = rep(0))
+datacols = c("C", "O")
+
+for(i in seq_along(treats)){
+  for(j in 1:2){
+    shapTable[i, j+1] = shapiro.test(subset(delta, lab == 'interlab' & type == treats[i] & iso == datacols[j])$value)$p.value
+  }
+}
+
+# One-sided t.tests for interlab comparisons
+diffTable = tTable = pTable = data.frame(treats, "dC" = rep(0), "dO" = rep(0))
+
+for(i in seq_along(treats)){
+  for(j in 1:2){
+      test = t.test(subset(delta, lab == 'interlab' & type == treats[i] & iso == datacols[j])$value)
+        #delta[delta$type == treats[i], datacols[j]])
+      diffTable[i, j+1] = round(test$estimate, 2)
+      tTable[i, j+1] = round(test$statistic, 2)
+      pTable[i, j+1] = round(test$p.value, 3)
+    }
+   }
+rm(datacols, treats, i, j, test)
 
